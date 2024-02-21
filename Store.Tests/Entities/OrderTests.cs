74465947220f046c3a975ac03c4be6a0d1856495
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-20 21:10:55 -0300

    namespace change

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,12 @@
+namespace Store.Tests.Domain;
+
+[TestClass]
+public class OrderTests
+{
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void TestMethod1()
+    {
+        Assert.Fail();
+    }
+}
---
+++ Store.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Store.Domain.Entities;
+namespace Store.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var cust = new Customer();
+    }
+}
